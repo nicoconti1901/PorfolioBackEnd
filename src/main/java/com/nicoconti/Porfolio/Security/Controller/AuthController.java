bb5e7ac9ca0f042faf87654c7f8f8cb0efeac010
nicoconti1901 <nico.conti.1901@gmail.com>
2023-01-30 19:47:53 -0300

    deploy junto con el frontend Enero23

--- src/main/java/com/nicoconti/Porfolio/Security/Controller/AuthController.java
+++ src/main/java/com/nicoconti/Porfolio/Security/Controller/AuthController.java
@@ -31,7 +31,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://porfoliobackendnicoconti.web.app")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
